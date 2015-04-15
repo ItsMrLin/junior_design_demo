b19a456400c0172ab13350ef484311fef3776ff9
ItsMrLin <JerryLin1206@Gmail.com>
2015-04-15 10:17:58 -0400

    initial migration

---
+++ nav.php
@@ -0,0 +1,24 @@
+<div id="custom-bootstrap-menu" class="navbar navbar-default " role="navigation">
+    <div class="container-fluid">
+        <div class="navbar-header"><a class="navbar-brand" href="#">InterviewBox</a>
+            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-menubuilder"><span class="sr-only">Toggle navigation</span><span class="icon-bar"></span><span class="icon-bar"></span><span class="icon-bar"></span>
+            </button>
+        </div>
+        <div class="collapse navbar-collapse navbar-menubuilder">
+            <ul class="nav navbar-nav navbar-left">
+                <li><a href="">Hi, <?php echo $_POST['name'];?></a>
+                </li>
+                <li><a href="">Feed</a>
+                </li>
+                <li><a href="">Profile</a>
+                </li>
+                <li><a href="">Schedule</a>
+                </li>
+                <li><a href="">Payment</a>
+                </li>
+                <li><a href="">Contact Us</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</div>
